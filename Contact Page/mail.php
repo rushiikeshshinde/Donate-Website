269dd36f40abcff838a9f13bf9357fdec285783f
MaryKroustali <71144000+MaryKroustali@users.noreply.github.com>
2020-12-20 20:26:59 +0200

    Add files via upload

---
+++ Contact Page/mail.php
@@ -0,0 +1,51 @@
+<?php 
+//php form for sending email via contact form
+
+//start session
+session_start();
+//include
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'C:/xampp1/COMPOSER/vendor/autoload.php';
+
+//get fields
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+
+//mail
+$mail = new PHPMailer();
+try {
+$mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+$mail->IsSMTP(); 
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true; 
+ //TO ENABLE ENTER GMAIL ADDRESS AND PASSWORD
+$mail->Username = '';                     
+$mail->Password = ''; 
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
+$mail->Port = 587;
+
+$mail->setFrom($email, $name);
+$mail->AddReplyTo($email, $name);
+$mail->addAddress('teozoumphs@gmail.com'); 
+$mail->SMTPOptions = array(
+'ssl' => array(
+'verify_peer' => false,
+'verify_peer_name' => false,
+'allow_self_signed' => true
+)
+);
+
+$mail->isHTML(true);
+$mail->Subject = 'Iris Contact Form';
+$mail->Body = $subject;
+
+$mail->send();  
+} 
+//errors
+catch (Exception $e){}
+
+?>
