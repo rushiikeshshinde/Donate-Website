7b652d915dfa893b2bdc748dedf83fd8957683bc
MaryKroustali <71144000+MaryKroustali@users.noreply.github.com>
2020-12-20 20:15:20 +0200

    Delete session.php

--- session.php
+++
@@ -1,9 +0,0 @@
-<?php
-//start session
-session_start();
-
-//check if user logged in
-if (isset($_SESSION["userid"]) && $_SESSION["userid"] === true ) {
- header("location: successLogin.html");
- exit;
-}
